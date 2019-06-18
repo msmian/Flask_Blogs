3ba8b43d63729fe87593b8583863d5d3a0d24a45
Your Name <you@example.com>
2019-06-18 20:13:51 +0500

    New Code

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:@localhost/python_for_all"
+
+db = SQLAlchemy(app)
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(20), nullable=False)
+    phone = db.Column(db.String(12), nullable=False)
+    msg = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=False)
 
 
 @app.route("/")
